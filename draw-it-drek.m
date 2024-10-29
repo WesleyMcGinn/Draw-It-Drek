ed85ebc23890413d6872af2702972be50391b616
Wesley <wesleymcginn1@gmail.com>
2024-10-29 15:11:29 -0400

    Make better comments

--- draw-it-drek.m
+++ draw-it-drek.m
@@ -1,13 +1,13 @@
-% DRAW (v2)
+% DRAW-IT DREK (v2)
 % Draw with a robotic arm controlled by a joystick
-% Written by Wes
+% Written by Carter, Omri, and Wesley
 % ENG1101 L34-4
-% 24 October 2024
+% 29 October 2024
 
 clc
 clear
 
-% 
+% Define components:
 a = arduino('COM11','Uno','Libraries','Servo');
 s1 = servo(a, 'D11');
 s2 = servo(a, 'D12');
@@ -16,16 +16,18 @@ X_JOYSTICK = 'A1';
 Y_JOYSTICK = 'A2';
 JOY_BUTTON = 'A3';
 
+% Define Constants:
 BASE_DRAWING_HEIGHT = -1;
 LIFT_HEIGHT = 2;
 
+% Declare variables:
 u = 5;
 v = 5;
 x = 5;
 y = 0;
 h = -1;
 
-while 1
+while 1 % Repeat the following forever:
     if (abs(a.readVoltage(X_JOYSTICK) - 2.5) > 0.3)
         x = x + readVoltage(X_JOYSTICK) - 2.5;
     end
@@ -49,6 +51,7 @@ while 1
     %end
 end
 
+% Writes an angle, from 0 to 180, to a servo:
 function servoWrite(Servo, Angle)
     if Angle < 0
         Angle = 0;
@@ -64,6 +67,7 @@ end
 %    possible = ((c * c + d * d) <= (u * u + 2 * u * v + v * v)) && ((c * c + d * d) >= (u * u - 2 * u * v + v * v));
 %end
 
+% The Fido Formulas, Version 3D, in MATLAB function format:
 % See https://www.desmos.com/3d/vgpartrk5s for explanation
 function [alpha, beta, omega] = roboArm(a, b, c, u, v)
     alpha = (pi * floor(sqrt(a^2 + c^2) / (u + v + 1)) + atan(b / sqrt(a^2 + c^2)) + acos((a^2 + b^2 + c^2 + u^2 - v^2)*sqrt(a^2 + b^2 + c^2) / (2*u*a*a + 2*u*b*b + 2*u*c*c))) * 180/pi;
