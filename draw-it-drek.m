ddc5fe0994f4b2a77de5b6b60da4ca55375a3014
Wesley <wesleymcginn1@gmail.com>
2024-10-30 21:47:05 -0400

    Make code work!

--- draw-it-drek.m
+++ draw-it-drek.m
@@ -9,8 +9,8 @@ clear
 
 % Define components:
 a = arduino('COM11','Uno','Libraries','Servo');
-s1 = servo(a, 'D11');
-s2 = servo(a, 'D12');
+s1 = servo(a, 'D12');
+s2 = servo(a, 'D11');
 s3 = servo(a, 'D13');
 X_JOYSTICK = 'A1';
 Y_JOYSTICK = 'A2';
@@ -18,7 +18,8 @@ JOY_BUTTON = 'A3';
 
 % Define Constants:
 BASE_DRAWING_HEIGHT = -1;
-LIFT_HEIGHT = 2;
+LIFT_HEIGHT = 0;
+DRAWINGSPEED = 0.05;
 
 % Declare variables:
 u = 5;
@@ -29,11 +30,12 @@ h = -1;
 
 while 1 % Repeat the following forever:
     if (abs(a.readVoltage(X_JOYSTICK) - 2.5) > 0.3)
-        x = x + a.readVoltage(X_JOYSTICK) - 2.5;
+        x = x + (a.readVoltage(X_JOYSTICK) - 2.5)*DRAWINGSPEED;
     end
     if (abs(a.readVoltage(Y_JOYSTICK) - 2.5) > 0.3)
-        y = y + a.readVoltage(Y_JOYSTICK) - 2.5;
+        y = y - (a.readVoltage(Y_JOYSTICK) - 2.5)*DRAWINGSPEED;
     end
+    fprintf("X: %3.2f, Y: %3.2f\n", x, y);
     if (a.readVoltage(JOY_BUTTON) == 0)
         if (h > BASE_DRAWING_HEIGHT)
             h = BASE_DRAWING_HEIGHT;
@@ -43,9 +45,9 @@ while 1 % Repeat the following forever:
         while (a.readVoltage(JOY_BUTTON) == 0)
         end % Wait until button is released
     end
-    if (inRange(x, y, u, v))
+    if (inRange(x, h, y, u, v))
         [alpha, beta, omega] = roboArm(x, h, y, u, v);
-        servoWrite(s1, alpha);
+        servoWrite(s1, 98 - alpha);
         servoWrite(s2, beta);
         servoWrite(s3, omega);
     end
@@ -71,5 +73,5 @@ end
 function [alpha, beta, omega] = roboArm(a, b, c, u, v)
     alpha = (pi * floor(sqrt(a^2 + c^2) / (u + v + 1)) + atan(b / sqrt(a^2 + c^2)) + acos((a^2 + b^2 + c^2 + u^2 - v^2)*sqrt(a^2 + b^2 + c^2) / (2*u*a*a + 2*u*b*b + 2*u*c*c))) * 180/pi;
     beta = (acos((u^2 + v^2 - a^2 - b^2 - c^2) / (2*u*v))) * 180/pi;
-    omega = (pi * floor(a / (u + v + 1)) + atan(c / a)) * 180/pi;
+    omega = (pi * floor(a / (u + v + 1)) + atan(c / a) + pi/2) * 180/pi;
 end
